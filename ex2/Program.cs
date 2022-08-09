89774cb58d04945f95b56b443a7c79e84ee5af40
Anton <j10r2@mail.ru>
2022-08-09 21:02:38 +0300

    exercises 2 & 3

---
+++ ex2/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = new Random().Next(100, 999);
+
+Console.WriteLine(a);
+
+int b = a / 100;
+int c = a % 10;
+
+Console.WriteLine(b);
+
+Console.WriteLine(c);
+
+Console.WriteLine(b * 10 + c);
---
+++ ex3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("введите первое число:");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine("введите второе число:");
+int b = int.Parse(Console.ReadLine());
+
+if (a % b == 0)
+{
+    Console.WriteLine("Первое число кратно второму");
+}
+else
+{
+    Console.WriteLine();
+    Console.WriteLine("остаток от деления равен " + a % b);
+}
