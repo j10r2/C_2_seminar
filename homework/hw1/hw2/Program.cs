b249f87c7521867a51c9621144e3da576a1fd4ab
Anton <j10r2@mail.ru>
2022-08-10 00:04:49 +0300

    задача 2

---
+++ homework/hw1/hw2/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите целое число (целое, пожалуйста):");
+while (true)
+{
+    try
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (Math.Abs(a) >= 100)
+                {   
+                    if (a > 999)
+                        {
+                        int i = 10;                    
+                        while (a / i > 1000)
+                            {i = i * 10;}
+                        Console.WriteLine("третья цифра числа - " + (a / i) % 10);
+                        }
+                    else
+                        {
+                        Console.WriteLine("третья цифра числа - " + a % 10);
+                        }
+                }
+            else
+                {
+                Console.WriteLine("в числе меньше трёх цифр");
+                }
+        break;
+        }
+catch
+    {
+        Console.WriteLine("Попробуйте ещё раз, цифры - это верхний ряд кнопок");
+        Console.WriteLine();
+    }
+}
