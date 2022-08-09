c2b920da27091429061f972235a42ef23f97762f
Anton <j10r2@mail.ru>
2022-08-09 21:35:32 +0300

    exercise 5

---
+++ ex5/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("введите первое число:");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine("введите второе число:");
+int b = int.Parse(Console.ReadLine());
+
+if (b == a * a)
+{
+    Console.WriteLine("второе число является квадратом первого");
+}
+else
+{
+    if (a == b * b)
+{
+    Console.WriteLine("первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("ни одно число не является квадратом другого");
+}
+}
