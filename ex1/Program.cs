d051751c4d7767c01abb1ee23552ef5f5f57ef82
Anton <j10r2@mail.ru>
2022-08-09 20:58:05 +0300

    exercise 1

---
+++ ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = new Random().Next(10, 100);
+Console.WriteLine(a);
+int b = a / 10;
+int c = a % 10;
+if (b == c)
+{
+    Console.WriteLine("числа равны");
+}
+else
+{
+if (b > c)
+{
+    Console.WriteLine(b);
+}
+else
+{
+    Console.WriteLine(c);
+}
+}
