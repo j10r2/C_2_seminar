3dcf6894374ccd12eae2dfd38cbccabf5b0c0a52
Anton <j10r2@mail.ru>
2022-08-09 22:33:38 +0300

    задача 1

---
+++ homework/hw1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите трёхзначное (целое) число:");
+while (true){
+try
+    {
+        int a = int.Parse(Console.ReadLine());
+        while ((Math.Abs(a) / 100 < 1) || (Math.Abs(a) / 100 >= 10))
+            {
+                Console.WriteLine();
+                Console.WriteLine("Введённое число не трёхзначное");
+                Console.WriteLine("сделай нормально");
+                a = int.Parse(Console.ReadLine());
+            }
+        Console.WriteLine("вторая цифра числа - " + (a % 100)/10);
+    break;
+    }
+catch
+    {
+        Console.WriteLine("Юзер, это не число, не зли машину о_0");
+        Console.WriteLine();
+    }
+}
