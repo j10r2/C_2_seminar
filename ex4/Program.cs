ebab68e03259d5a340c1a189e3dd8a100cfecd7b
Anton <j10r2@mail.ru>
2022-08-09 21:34:49 +0300

    exercise 4

---
+++ ex4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine());
+if (a % 7 == 0 & a % 23 ==0)
+{
+    Console.WriteLine("число кратно и 7 и 23-м");
+}
+else
+{
+    Console.WriteLine("пшёл вон пёс");
+}
